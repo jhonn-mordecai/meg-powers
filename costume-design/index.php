8da0a6c16110a16cc11216898156396f312b757c
John Mordecai <john.mordecai@gmail.com>
2024-01-02 21:16:01 -0500

    Add list of links to Costume Design index

--- costume-design/index.php
+++ costume-design/index.php
@@ -3,11 +3,26 @@
 
 <div class="fluid-container">
   <section class="project-headings">
-    <h1>Nothing here.</h1>
-    <p>
-      Maybe just <a href="<?= $BASE_URL; ?>" title="home">go back home</a>.
-    </p>
+    <h1>
+        <span class="sr-only">Costume Design </span>
+        <span class="icon-costume-dsn"></span>
+    </h1>
   </section>
+    <p>View photos and illustrations for each production/concept via the links below.</p>
+    <h2>Productions</h2>
+    <ul class="index-list">
+      <li><a href="<?= $BASE_URL; ?>/costume-design/alice" title="Alice production photos">Alice</a></li>
+      <li><a href="<?= $BASE_URL; ?>/costume-design/cabaret" title="Cabaret production photos">Cabaret</a></li>
+      <li><a href="<?= $BASE_URL; ?>/costume-design/twelfth-night" title="Twelfth Night production photos">Twelfth Night</a></li>
+    </ul>
+    <h2>Theoretical Concepts</h2>
+    <ul class="index-list">
+      <li><a href="<?= $BASE_URL; ?>/costume-design/pinocchio" title="The Adventures of Pinocchio concepts">The Adventures of Pinocchio</a></li>
+      <li><a href="<?= $BASE_URL; ?>/costume-design/airline-highway" title="Airline Highway concepts">Airline Highway</a></li>
+      <li><a href="<?= $BASE_URL; ?>/costume-design/one-flea-spare" title="One Flea Spare concepts">One Flea Spare</a></li>
+      <li><a href="<?= $BASE_URL; ?>/costume-design/peer-gynt" title="Peer Gynt concepts">Peer Gynt</a></li>
+      <li><a href="<?= $BASE_URL; ?>/costume-design/sun-is-sinking-in-the-west" title="The Sun is Sinking in the West concepts">The Sun is Sinking in the West</a></li>
+    </ul>
 </div>
 
 <?php include('../components/footer.php'); ?>

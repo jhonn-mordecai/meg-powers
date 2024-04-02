22f3c7b2cf1cde8f3fa26a2da972aa0da9069423
John Mordecai <john.mordecai@gmail.com>
2024-04-01 20:34:48 -0400

    Add info blurb to project page

--- costume-design/sun-is-sinking-in-the-west.php
+++ costume-design/sun-is-sinking-in-the-west.php
@@ -10,7 +10,9 @@
         <span class="project-title">The Sun Is Sinking In the West</span>
     </h1>
     <div class="project-details">
-        <p></p>
+        <p>Playwright: Steven Glavey</p>
+        <p>THE SUN IS SINKING IN THE WEST is a new play written by a longtime  collaborator, Steven Glavey. As much an intimate family drama as a horror story, this play explores the grim complicities of the Westward Expansion through vampire folklore and takes inspiration from Aleksey Tolstoy, spaghetti westerns, and giallo.</p>
+        <!-- <p>This was a rare and fulfilling opportunity to develop the costumes in direct conversation with the playwright, and we are both very happy with the results. To see a fully realized build of Lilybell Glanton's Look 3 dress, <a href="" title="View photos of Lilybell Glanton's Look 3 dress">click here</a>.</p> -->
     </div>
   </section>
   <div class="project-gallery">
